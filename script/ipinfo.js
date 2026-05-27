42b57d2214e68dde8be920953d20608907a14555
liwang0918-collab <liwang0918@gmail.com>
2026-05-27 21:15:32 +0800

    Add IP info retrieval from ip-api.com

--- script/ipinfo.js
+++ script/ipinfo.js
@@ -1,1 +1,24 @@
+const url = “http://ip-api.com/json/?lang=zh-CN”;
 
+$httpClient.get(url, function(error, response, data) {
+
+let obj = JSON.parse(data);
+
+let result =
+`节点信息查询
+
+IP：${obj.query}
+国家：${obj.country}
+地区：${obj.regionName}
+城市：${obj.city}
+ISP：${obj.isp}
+ASN：${obj.as}`;
+
+$done({
+response: {
+status: 200,
+body: result
+}
+});
+
+});
