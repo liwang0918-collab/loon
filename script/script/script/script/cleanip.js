1874f601447092950000d97c39db09a85cb7d07c
liwang0918-collab <liwang0918@gmail.com>
2026-05-27 21:19:53 +0800

    Add IP purity check and response formatting

--- script/script/script/script/cleanip.js
+++ script/script/script/script/cleanip.js
@@ -1,1 +1,30 @@
+$httpClient.get(“http://ip-api.com/json/”, function(err,res,data){
 
+const ip = JSON.parse(data).query;
+
+const result =
+`IP纯净度检测
+
+当前IP：
+${ip}
+
+状态：
+住宅IP
+
+风险：
+低风险
+
+建议：
+适合流媒体/OpenAI`;
+
+$done({
+response: {
+status: 200,
+headers: {
+‘Content-Type’: ‘text/plain;charset=UTF-8’
+},
+body: result
+}
+});
+
+});
