6f776b6f4aee98151d3b0546d7c8c6e91e4f1f10
liwang0918-collab <liwang0918@gmail.com>
2026-05-27 21:18:18 +0800

    Implement unlock detection for multiple streaming services
    Added functionality to check unlock status of various streaming services.

--- script/script/unlock.js
+++ script/script/unlock.js
@@ -1,1 +1,54 @@
+const media = [
+{
+name: “Netflix”,
+url: “https://www.netflix.com/title/81215567”
+},
+{
+name: “Disney+”,
+url: “https://www.disneyplus.com”
+},
+{
+name: “TikTok”,
+url: “https://www.tiktok.com”
+},
+{
+name: “OpenAI”,
+url: “https://chat.openai.com”
+}
+];
 
+let results = [];
+let finished = 0;
+
+media.forEach(item => {
+
+$httpClient.get(item.url, function(error, response, data) {
+
+if (!error && response.status == 200) {
+results.push(${item.name}：已解锁);
+} else {
+results.push(${item.name}：未解锁);
+}
+
+finished++;
+
+if (finished === media.length) {
+
+$done({
+response: {
+status: 200,
+headers: {
+‘Content-Type’: ‘text/plain;charset=UTF-8’
+},
+body:
+`流媒体解锁检测
+
+${results.join(”\n”)}`
+}
+});
+
+}
+
+});
+
+});
