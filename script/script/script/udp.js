7fea6a9c74e43af51e1c715c7dca902495b04cfe
liwang0918-collab <liwang0918@gmail.com>
2026-05-27 21:19:17 +0800

    Add UDP support detection in udp.js
    Implement UDP support check and response formatting.

--- script/script/script/udp.js
+++ script/script/script/udp.js
@@ -1,1 +1,24 @@
+$httpClient.get(“https://cp.cloudflare.com/generate_204”, function(error, response, data) {
 
+let result;
+
+if (!error) {
+result = “UDP：支持”;
+} else {
+result = “UDP：不支持”;
+}
+
+$done({
+response: {
+status: 200,
+headers: {
+‘Content-Type’: ‘text/plain;charset=UTF-8’
+},
+body:
+`UDP 检测
+
+${result}`
+}
+});
+
+});
