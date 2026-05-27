cf025ccd577c5a68c844234c32ed9088eaa08766
liwang0918-collab <liwang0918@gmail.com>
2026-05-27 21:20:48 +0800

    Implement ping function to measure latency
    Add ping functionality to measure response time.

--- script/script/script/script/script/ping.js
+++ script/script/script/script/script/ping.js
@@ -1,1 +1,21 @@
+const start = Date.now();
 
+$httpClient.get(“https://www.gstatic.com/generate_204”, function(error, response, data) {
+
+const ms = Date.now() - start;
+
+$done({
+response: {
+status: 200,
+headers: {
+‘Content-Type’: ‘text/plain;charset=UTF-8’
+},
+body:
+`节点测速
+
+延迟：
+${ms} ms`
+}
+});
+
+});
